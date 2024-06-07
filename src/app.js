ab7954d86df224bab592226678c79bad2d76772c
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-06-07 15:42:47 +1000

    feat: hello world with bruno routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 // Declare and configure the server
+
+const express = require('express');
+const serverInstance = express();
+
+// Raw JSON in body allowed
+serverInstance.use(express.json());
+// Form data in body allowed
+serverInstance.use(express.urlencoded({extended: true}));
+
+serverInstance.get('/', (request, response) => {
+    console.log("Someone visited the homepage of the server")
+
+    response.json({
+        message: "Hello World! Dillon was here."
+    })
+})
+
+serverInstance.post("/", (request, response) => {
+
+    console.log(request.body)
+
+    response.json({
+        message: "Received data:",
+        requestData: request.body
+    })
+})
+
+// To make the instance available for other files to use
+module.exports = serverInstance;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // Run the server
+
+const serverInstance = require("./app");
+
+
+serverInstance.listen(3000, () => {
+    console.log("Server is running")
+})
